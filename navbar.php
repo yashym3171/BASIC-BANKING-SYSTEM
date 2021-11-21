04dbad65c51a89355eab571d79dfc68423f26c88
yashym3171 <93377707+yashym3171@users.noreply.github.com>
2021-11-21 17:15:07 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,48 @@
+<!-- navbar --> 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- custome -->
+    <link rel="stylesheet" href="./css/navbar.css">
+    
+  </head>
+  <style>
+    body{
+        background-image:url("./img/back pink.jpg");
+        background-repeat:no-repeat;
+        background-size:100%;
+    }
+    </style>
+
+<body>
+<!-- Start Navigation  -->
+<header class="header_area">
+  <div class="main-menu">
+      <nav class="navbar navbar-expand-lg navbar-light">
+         <!-- <a class="navbar-brand" class = "img"href="index.php">
+            <img src="./img/Bank_logo.png" alt="logo" width="50px" height="50px"></a> -->
+          <div class="collapse navbar-collapse" id="navbarNav">
+              <div class="mr-auto"></div>
+              <ul class="navbar-nav">
+                  <li class="nav-item active">
+                      <a class="nav-link" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#services">Services</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#about">About</a>
+                  </li>
+
+              </ul>
+          </div>
+      </nav>
+  </div>
+</header>
+<!-- End Navigation  -->
+</body>
+</html>
